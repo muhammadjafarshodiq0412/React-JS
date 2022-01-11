bfc8cb9dfb911a350cb495affe39afddda795758
jafar1204 <muhammad.shodiq@pgi-data.com>
2022-01-11 08:41:10 +0700

    -ADD Functional Component

---
+++ src/components/Greet.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello World!</h1>
+// }
+
+// With Pattern ES6
+const Greet = () => <h1>Hello World!</h1>
+
+export default Greet;
